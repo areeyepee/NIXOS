e89ad60f4c2b78eb06e4d4b83c273647733737f2
Raphael <rp@proton.me>
2025-08-01 09:54:16 +0200

    Added Nix Files from Laptop.

---
+++ NixLaptop/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    # This is pointing to an unstable release.
+    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
+    # i.e. nixos-24.11
+    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = inputs@{ self, nixpkgs, ... }:
+  	let
+  	system = "x86_64-linux";
+  #	hostname = "NixLaptop";
+  #	username = "areeyepee";
+  	in
+  	{
+    # NOTE: 'nixos' is the default hostname
+  #  inherit hostname;
+    nixosConfigurations.NixLaptop = nixpkgs.lib.nixosSystem {
+
+      inherit system;
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
+
